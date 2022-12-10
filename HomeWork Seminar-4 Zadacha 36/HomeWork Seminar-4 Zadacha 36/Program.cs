c6979a53045c728cf3bd0aa716e16222c0638b8c
Alexandr <hitrin79@gmail.com>
2022-12-10 20:04:32 +0800

    Zadacha 36

---
+++ HomeWork Seminar-4 Zadacha 36/HomeWork Seminar-4 Zadacha 36/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int size = 20;
+
+int[] array = new int[size];
+
+void PrintArray(int[] arr)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+}
+
+void SummArray(int[] sum)
+{
+    int arraySum = 0;
+    
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        arraySum = arraySum + array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма элементов на нечетных позициях: {arraySum}");  
+}
+
+
+FillArray(array);
+PrintArray(array);
+SummArray(array);
